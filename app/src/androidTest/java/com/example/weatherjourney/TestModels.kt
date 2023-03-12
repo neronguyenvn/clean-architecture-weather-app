e50246dc6423a6421a4063f99fe66d591e8ebd2b
nero <hungnd.vnse@gmail.com>
2023-03-12 19:20:30 +0700

    comment test codes

--- app/src/androidTest/java/com/example/weatherjourney/TestModels.kt
+++ app/src/androidTest/java/com/example/weatherjourney/TestModels.kt
@@ -1,10 +1,9 @@
 package com.example.weatherjourney
 
-import com.example.weatherjourney.weather.data.local.entity.LocationEntity
-import com.example.weatherjourney.weather.domain.model.Coordinate
-
+/*
 const val city1 = "Thu Duc City"
 val coordinate1 = Coordinate(10.873, 106.742)
 
 val location1 = LocationEntity(city1, coordinate1.lat, coordinate1.long)
 val location2 = LocationEntity("Usa", 39.784, -100.446)
+*/
